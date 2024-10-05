b364b703044c9daecca2516e4a69392e2a27f77e
Amit Kumar Sahu <135963282+amitks20@users.noreply.github.com>
2024-10-05 19:38:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const nav = document.querySelector('nav');
+//     const toggleButton = document.createElement('button');
+
+//     toggleButton.classList.add('nav-toggle');
+//     nav.insertBefore(toggleButton, nav.firstChild);
+  
+//     toggleButton.addEventListener('click', () => {
+//       nav.classList.toggle('active');
+//     });
+//   });
+  
